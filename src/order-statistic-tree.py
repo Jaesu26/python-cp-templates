15c00b32d0c5362444b2e521fca2d4de94ba6bdf
gkswotn12345 <84672164+Jaesu26@users.noreply.github.com>
2026-09-18 13:05:35 +0900

    [Refactor] Use start parameter in enumerate

--- src/order-statistic-tree.py
+++ src/order-statistic-tree.py
@@ -10,8 +10,8 @@ class OrderStatisticTree:
             self._build(max_value_or_counts)
 
     def _build(self, array):
-        for i, a in enumerate(array):
-            self._tree[i + self._size] = a
+        for i, a in enumerate(array, start=self._size):
+            self._tree[i] = a
         for i in range(self._size - 1, 0, -1):
             self._tree[i] = self._tree[i << 1] + self._tree[i << 1 | 1]
 
